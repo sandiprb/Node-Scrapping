62d5d3e6e04ae49edc37a3013dafdbd9331d981f
sandiprb <sandip@coverfoxmail.com>
2016-10-15 20:18:05 +0530

    Movie list working

---
+++ app.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var path = require('path');
+
+
+var app = express();
+
+var nunjucks  = require('nunjucks');
+var routes = require('./routes/index');
+
+// view engine setup
+app.set('view engine', 'html');
+nunjucks.configure(path.join(__dirname, 'templates'), {
+    autoescape: true,
+    express: app
+});
+
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'static')));
+app.use(session({secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
+
+/*APP Routes*/
+app.use('/', require('./routes/index'));
+app.use('/top/movies', require('./routes/TopMovies'));
+app.use('/movie', require('./routes/movie'));
+
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.send({
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.send({
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
---
+++ routes/TopMovies.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var request = require('request');
+var cheerio = require('cheerio');
+var fs = require('fs');
+var router = express.Router();
+
+var BASE = 'http://www.imdb.com'
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	var movies = []
+	request.get(BASE+'/chart/top', function(err, result, body){
+		var $ = cheerio.load(body)
+		var links = $(".lister-list > tr").map(function(i){
+			var movie = {
+				rank: (movies.length + 1),
+				title: $(this).find(".titleColumn > a").text(),
+				year: $(this).find(".titleColumn > span").text(),
+				rating:  + ($(this).find(".ratingColumn.imdbRating > strong").text()), 
+				votes: $(this).find(".ratingColumn.imdbRating > strong").attr("title").split("on")[1].split("user")[0].trim(),
+				link: (BASE + $(this).find(".titleColumn > a").attr("href").split("?")[0]),
+				imgURL: $(this).find(".posterColumn").find("a > img").attr("src")
+			}
+			movies.push(movie)
+		}).get();	
+		fs.writeFile('scrap.json', JSON.stringify(movies),  function(err) {
+			if (err) {
+				return console.error(err);
+			}
+		})
+
+	res.render('topmovies', {
+		title: "IMDB Top 250 movies",
+		movies: movies
+	})
+	}) 
+
+})
+
+module.exports = router;
